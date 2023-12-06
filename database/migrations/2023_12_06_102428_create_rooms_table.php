98700cd9d7f4dc055e1593a7b29aa9e19e39ac61
estradax <azharumuhammad@gmail.com>
2023-12-06 17:30:44 +0700

    add room and reservation

---
+++ database/migrations/2023_12_06_102428_create_rooms_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->char('public_id', 15);
+            $table->integer('bed_count')->unsigned();
+            $table->decimal('price')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
