70b9be4c8d635d4abbf36e54f0efff1eb7b90df8
estradax <azharumuhammad@gmail.com>
2023-12-07 21:52:24 +0700

    add reservation

---
+++ app/Http/Controllers/ReservationController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ReservationController extends Controller
+{
+    public function index()
+    {
+        return view('reservation');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ReservationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,4 @@ Route::get('/', function () {
 
 Route::get('/admin', DashboardController::class)->name('admin.dashboard.index');
 Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::get('/reservation', [ReservationController::class, 'index'])->name('reservation');
